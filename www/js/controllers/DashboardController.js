affe1d9297f278144125231dc8cdea52e528c2e7
kathyliang <liang9503@gmal.com>
2015-12-15 23:12:01 -0500

    add all file

---
+++ www/js/controllers/DashboardController.js
@@ -0,0 +1,95 @@
+angular.module('MetronicApp').controller('DashboardController', function($rootScope, $scope, $http, $timeout,$interval) {
+    var DashCtrl = this;
+    $scope.$on('$viewContentLoaded', function() {   
+        // initialize core components
+        App.initAjax();
+    });
+
+    // set sidebar closed and body solid layout mode
+    $rootScope.settings.layout.pageContentWhite = true;
+    $rootScope.settings.layout.pageBodySolid = false;
+    $rootScope.settings.layout.pageSidebarClosed = true;
+    
+    DashCtrl.hello = "hello";
+    function get_orders() {
+        $http({
+          method: 'GET',
+          url: 'https://www.chanmao.ca/index.php?r=MobMonitor/OrderList',
+          headers: {
+           'Authortoken': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiIxMDYxMSIsImV4cGlyZWQiOjE0NTQzMDE3ODd9.Zanu0l3LW31UlGZ72PXbcBUDPGOstKai2oMYiX4ab_Y'
+         },
+        }).then(function successCallback(response) {
+            console.log(response)
+            DashCtrl.orders = response.data.ea_orders;
+            DashCtrl.statas = response.data.ea_stats;
+             setOrders()   
+          }, function errorCallback(response) {
+           // alertService.alert(response);
+          });
+        function setOrders() {
+
+            DashCtrl.new_order = [];
+            DashCtrl.change_addr_order = [];
+            DashCtrl.new_user_order = [];
+            DashCtrl.reject_order = [];
+            DashCtrl.confirm_order = [];
+            DashCtrl.delivering_order = [];
+            DashCtrl.complete_order = [];
+            DashCtrl.drivers = [];
+
+            _.forEach( DashCtrl.orders, function(order, key) {
+              // console.log(order, key);
+                switch(order.status) {
+                  case '0':
+                      DashCtrl.new_order.push(order)
+                      break;
+                  case '60':
+                       DashCtrl.change_addr_order.push(order)
+                      break;
+                  case '5':
+                       DashCtrl.reject_order.push(order)
+                      break;
+                  case '55':
+                       DashCtrl.new_user_order.push(order)
+                      break;
+                  case '10':
+                      DashCtrl.confirm_order.push(order)
+                      break;
+                  case '20':
+                       DashCtrl.delivering_order.push(order)
+                      break;
+                  case '30':
+                       DashCtrl.delivering_order.push(order)
+                      break;
+                  case '40':
+                       DashCtrl.complete_order.push(order)
+                      break; 
+                }
+            }); 
+            
+            _.forEach(DashCtrl.delivering_order,function(order) {
+               var driver_index = _.findIndex(DashCtrl.drivers, function(driver) {
+                  return driver.deliver == order.deliver;
+                });
+               // console.log('driver',driver_index)
+                if(driver_index == '-1'){
+                    var driver = {}
+                    driver.deliver = order.deliver;
+                    driver.orders = [];
+                    driver.orders.push(order)
+                    DashCtrl.drivers.push(driver)
+                }else{
+                    DashCtrl.drivers[driver_index].orders.push(order)
+                }
+            });  
+            console.log(DashCtrl.delivering_order) 
+            console.log("1", DashCtrl.drivers)
+        };
+    };
+
+
+    $interval(function() { 
+        get_orders();
+    },30000)
+    get_orders();
+});
